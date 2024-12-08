5c06aa937b9d17a9e8f3120c4aa85b35472b4c42
avaerian <55154263+avaerian@users.noreply.github.com>
2024-12-08 02:07:38 -0500

    Continue work for embed creation system + general useful bot API

--- src/commands/embed_creator.rs
+++ src/commands/embed_creator.rs
@@ -1,34 +1,97 @@
-use std::sync::Arc;
+use std::fmt::Error;
+use std::str::FromStr;
 use crate::command::{CommandContext, CommandHandler};
-use serenity::all::{ButtonStyle, CommandInteraction, ComponentInteraction, Context, CreateActionRow, CreateCommand, CreateInputText, CreateInteractionResponse, CreateInteractionResponseMessage, CreateModal, CreateQuickModal, InputTextStyle};
-use serenity::{async_trait, Error};
+use crate::component::ComponentCallback;
+use crate::component::ComponentContext;
+use serenity::all::{ButtonStyle, CreateActionRow, CreateCommand, CreateInputText, CreateInteractionResponse, CreateInteractionResponseMessage, CreateModal, InputTextStyle};
 use serenity::builder::CreateButton;
-use crate::bot::BotData;
+use serenity::{async_trait, Error};
+
+
+/*enum EmbedButton {
+    Author,
+    Title,
+    Description,
+    Color,
+    Image,
+    Thumbnail,
+    Footer,
+
+    // Submit
+    // Cancel
+}*/
+
+// review https://stackoverflow.com/questions/37006835/building-an-enum-inside-a-macro for more complex enums for macros
+macro_rules! define_embed_btns {
+    ( $($v:vis,)? $($btn:ident),+ ) => {
+        v enum EmbedButton {
+            $($btn),+
+        }
+
+        impl FromStr for EmbedButton {
+            type Err = ();
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                match s.to_lowercase() {
+                    $(
+                       str::to_lowercase(stringify!($btn)) => { Ok(EmbedButton::$btn) }
+                    )+
+                    _ => { Err(())}
+                }
+            }
+        }
+
+        /*impl Into<String> for EmbedButton {
+
+        }*/
+
+        // TODO: Into<String>, Iterator, size, additional fns for associated data
+    };
+}
+
+/*
+    To associate values/data with the enum, the macro will generate
+    functions with match statements that return the appropriate data for each enum
+*/
+
+fn test() {
+    match EmbedButton::from_str("author") {
+        Ok(btn_type) => {}
+    }
+}
+
+// TODO: refactor to define_btns! macro (contain namespace, custom_id, etc.)
+define_embed_btns!(Author, Title, Description, Color, Image, Thumbnail, Footer, Submit, Cancel);
+
+struct EmbedButtonCallback;
+
+#[async_trait]
+impl ComponentCallback for EmbedButtonCallback {
+    async fn run<'a>(&self, ctx: ComponentContext<'a>) -> Result<(), Error> {
+        let interaction = ctx.interaction;
+        let btn_id = &interaction.data.custom_id;
+        let modal = match btn_id.as_str() {
+            "author" => {
+                let set_author_field = CreateActionRow::InputText(CreateInputText::new(InputTextStyle::Short, "Set embed author", "embed:author"));
+                let mut comps = Vec::new();
+                comps.push(set_author_field);
+                CreateModal::new("modal:author", "Test modal")
+                    .components(comps)
+            }
+            "title" => {
+                let set_title_field = CreateActionRow::InputText(CreateInputText::new(InputTextStyle::Short, "Set embed title", "embed:title"));
+                let mut comps = Vec::new();
+                comps.push(set_title_field);
+                CreateModal::new("modal:title", "Test modal")
+                    .components(comps)
+            }
+            _ => unimplemented!()
+        };
+
+        interaction.create_response(&ctx.ctx.http, CreateInteractionResponse::Modal(modal)).await
+    }
+}
 
 pub struct EmbedCreatorCommand;
-
-pub async fn handle_button(bot: Arc<BotData>, ctx: &Context, interaction: &ComponentInteraction) -> Result<(), Error> {
-    let btn_id = &interaction.data.custom_id;
-    let modal = match btn_id.as_str() {
-        "author" => {
-            let set_author_field = CreateActionRow::InputText(CreateInputText::new(InputTextStyle::Short, "Set embed author", "embed:author"));
-            let mut comps = Vec::new();
-            comps.push(set_author_field);
-            CreateModal::new("modal:author", "Test modal")
-                .components(comps)
-        }
-        "title" => {
-            let set_title_field = CreateActionRow::InputText(CreateInputText::new(InputTextStyle::Short, "Set embed title", "embed:title"));
-            let mut comps = Vec::new();
-            comps.push(set_title_field);
-            CreateModal::new("modal:title", "Test modal")
-                .components(comps)
-        }
-        _ => unimplemented!()
-    };
-
-    Ok(())
-}
 
 #[async_trait]
 impl CommandHandler for EmbedCreatorCommand {
@@ -37,14 +100,17 @@ impl CommandHandler for EmbedCreatorCommand {
             .dm_permission(false)
     }
 
-    async fn run(&self, ctx: CommandContext) -> Result<(), Error> {
+    async fn run<'a>(&self, ctx: CommandContext<'a>) -> Result<(), Error> {
         let bot = ctx.bot;
         let interaction = ctx.interaction;
         let http = &ctx.ctx.http;
 
+        // TODO: Create namespaces for component system !!
+
         // TODO: eventually refactor into EmbedTemplate for storing on server/dumping to file
         // TODO: introduce caching for these types of messages? (not priority)
         let mut edit_btns: Vec<CreateButton> = Vec::new(); // for caching, associate with id and if None, add buttons and cache result
+        // EmbedButton::Author::create().label("Set Author") ... ??
         edit_btns.push(CreateButton::new("author").label("Set Author").style(ButtonStyle::Secondary));
         edit_btns.push(CreateButton::new("title").label("Set Title").style(ButtonStyle::Secondary));
         edit_btns.push(CreateButton::new("desc").label("Set Description").style(ButtonStyle::Secondary));
@@ -53,7 +119,7 @@ impl CommandHandler for EmbedCreatorCommand {
         edit_btns.push(CreateButton::new("thumbnail").label("Set Thumbnail").style(ButtonStyle::Secondary));
         edit_btns.push(CreateButton::new("footer").label("Set Footer").style(ButtonStyle::Secondary));
 
-        bot.components.register_component("author", handle_button).await;
+        bot.components.register_component("author", EmbedButtonCallback).await; // move to register ?
 
         let mut submit_btns: Vec<CreateButton> = Vec::new();
         submit_btns.push(CreateButton::new("cancel").label("Cancel").style(ButtonStyle::Danger));
